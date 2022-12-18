990ec9ba10fb7f8b1deb47ed6f797f5c924c0af6
Emmanuel <e.gutierrezrivera02@gmail.com>
2022-12-18 17:30:33 -0500

    added a database

--- PublicWeb-DS/MainPage.js
+++ PublicWeb-DS/MainPage.js
@@ -5,13 +5,13 @@ async function saveData() {
     let password = document.getElementById('pswd').value;
     
     const data = { username, password };
-        const  options = {
-            method: 'POST',
-            headers: { // specifies that we are sending the data in a json format
-                'content-Type': 'application/json'
-            },
-            body: JSON.stringify(data) // where we package all of the data
-        };
+    const  options = {
+        method: 'POST',
+        headers: { // specifies that we are sending the data in a json format
+            'content-Type': 'application/json'
+        },
+         body: JSON.stringify(data) // where we package all of the data
+    };
     const response = await fetch('/api', options);
     const json = await response.json();
     console.log(json);
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express'); // basically like an inport statement 
+const datastore = require('nedb'); // imports the nedb database so it is required
 const app = express(); // holds the whole node express library
 
 app.listen(3000, () => console.log('Listening at 3000')); // listens to a port (a unique number at which we listen)
-app.use(express.static('PublicWeb-DS'));
-app.use(express.static('index.html')) // Takes in a folder or file name (which is static, like a html file)
+app.use(express.static('PublicWeb-DS')); // Takes in a folder or file name (which is static, like a html file)
 
 app.use(express.json({limit: '1mb'})); // helps the server understand incoming data that is in json format
+
+const database = new datastore('database.db'); //creates a database with given name file
+database.loadDatabase(); // creates the file if its not already created
+database.insert({name: 'emmanuel', status: 'chilling'})
 
 // post request. specify the adress
 // request - holds everything that's contained within that request, (all the data that is being sent)
@@ -13,6 +17,8 @@ app.use(express.json({limit: '1mb'})); // helps the server understand incoming d
 app.post('/api', (request, response) => {
     console.log(request.body);
     const data = request.body;
+    database.insert(data);// save the data in the datasore file
+    console.log(database);
     response.json({
         status: 'success',
         username: data.username,
