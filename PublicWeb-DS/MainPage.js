4d403cedd7d0140c592d40c5d55532932ba5deea
Emmanuel <e.gutierrezrivera02@gmail.com>
2022-12-20 01:33:26 -0500

    AJAX form submission

--- PublicWeb-DS/MainPage.js
+++ PublicWeb-DS/MainPage.js
@@ -3,6 +3,25 @@ async function saveData() {
     // get username and password from placeholders
     let username = document.getElementById('uName').value;
     let password = document.getElementById('pswd').value;
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application//x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        })
+        .then(() => console.log("Form successfully submitted"))
+        .catch((error) => alert(error));
+    };
+
+    document
+        .querySelector("form")
+        .addEventListener("submit", handleSubmit);
     
     const data = { username, password };
     const  options = {
