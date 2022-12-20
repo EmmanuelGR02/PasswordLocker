4fe4e1143b5f7c18d166350278741a6063670763
Emmanuel <e.gutierrezrivera02@gmail.com>
2022-12-20 01:53:00 -0500

    test3

--- PublicWeb-DS/MainPage.js
+++ PublicWeb-DS/MainPage.js
@@ -9,7 +9,7 @@ async function saveData() {
     const  options = {
         method: 'POST',
         headers: { // specifies that we are sending the data in a json format
-            'content-Type': 'application/json'
+            'content-Type': 'application/x-www-form-urlencoded'
         },
         body: new URLSearchParams(data).toString()
         //body: JSON.stringify(data) // where we package all of the data
