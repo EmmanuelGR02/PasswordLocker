3e1db281294e8e46cf5136ad0b696b316b414e71
Emmanuel <e.gutierrezrivera02@gmail.com>
2022-12-20 01:44:39 -0500

    AJAX form submission 2

--- PublicWeb-DS/MainPage.js
+++ PublicWeb-DS/MainPage.js
@@ -4,35 +4,26 @@ async function saveData() {
     let username = document.getElementById('uName').value;
     let password = document.getElementById('pswd').value;
 
-    const handleSubmit = (event) => {
+    
+    const handleSubmit = async (event) => {
         event.preventDefault();
-
         const myForm = event.target;
         const formData = new FormData(myForm);
-
-        fetch("/api", {
-            method: "POST",
-            headers: { "Content-Type": "application//x-www-form-urlencoded" },
-            body: new URLSearchParams(formData).toString(),
-        })
-        .then(() => console.log("Form successfully submitted"))
-        .catch((error) => alert(error));
+        const data = { username, password };
+        const  options = {
+            method: 'POST',
+            headers: { // specifies that we are sending the data in a json format
+                'content-Type': 'application/json'
+            },
+            body: new URLSearchParams(formData).toString()
+            //body: JSON.stringify(data) // where we package all of the data
+        };
+        const response = await fetch('/api', options);
+        const json = await response.json();
+        console.log(json);
     };
-
     document
         .querySelector("form")
         .addEventListener("submit", handleSubmit);
-    
-    const data = { username, password };
-    const  options = {
-        method: 'POST',
-        headers: { // specifies that we are sending the data in a json format
-            'content-Type': 'application/json'
-        },
-         body: JSON.stringify(data) // where we package all of the data
-    };
-    const response = await fetch('/api', options);
-    const json = await response.json();
-    console.log(json);
 }
 
