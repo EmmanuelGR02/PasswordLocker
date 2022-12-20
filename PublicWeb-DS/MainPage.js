9d617eeb1d4d0ea48fffe48f360e8a9c7667c70a
Emmanuel <e.gutierrezrivera02@gmail.com>
2022-12-20 01:49:26 -0500

    test2

--- PublicWeb-DS/MainPage.js
+++ PublicWeb-DS/MainPage.js
@@ -4,26 +4,18 @@ async function saveData() {
     let username = document.getElementById('uName').value;
     let password = document.getElementById('pswd').value;
 
-    
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const myForm = event.target;
-        const formData = new FormData(myForm);
-        const data = { username, password };
-        const  options = {
-            method: 'POST',
-            headers: { // specifies that we are sending the data in a json format
-                'content-Type': 'application/json'
-            },
-            body: new URLSearchParams(formData).toString()
-            //body: JSON.stringify(data) // where we package all of the data
-        };
-        const response = await fetch('/', options);
-        const json = await response.json();
-        console.log(json);
+       
+    const data = { username, password };
+    const  options = {
+        method: 'POST',
+        headers: { // specifies that we are sending the data in a json format
+            'content-Type': 'application/json'
+        },
+        body: new URLSearchParams(data).toString()
+        //body: JSON.stringify(data) // where we package all of the data
     };
-    document
-        .querySelector("form")
-        .addEventListener("submit", handleSubmit);
+    const response = await fetch('/api', options);
+    const json = await response.json();
+    console.log(json);
 }
 
